b111c81a2f365124763953fa880984783d5c0fc9
asirago <40822894+asirago@users.noreply.github.com>
2024-03-01 22:55:17 +0100

    add whichkey nvim plugin

---
+++ nvim/lua/asirago/plugins/whichkey.lua
@@ -0,0 +1,20 @@
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 500
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        triggers_blacklist = {
+            -- list of mode / prefixes that should never be hooked by WhichKey
+            -- this is mostly relevant for keymaps that start with a native binding
+            i = { "j", "k", "v", "l", "h" },
+            v = { "j", "k", "v", "l", "h" },
+            n = { "v" },
+        },
+    },
+}
